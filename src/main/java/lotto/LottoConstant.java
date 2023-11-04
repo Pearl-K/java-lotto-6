9d2b8356cf3cd5163611e83661956f22d15dfa38
Pearl <rkdwlswn227@gmail.com>
2023-11-04 17:33:40 +0900

    feat: LottoConstant 로또 기능에 쓰이는 상수 enum 클래스로 관리

---
+++ src/main/java/lotto/LottoConstant.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public enum LottoConstant {
+
+    MIN_NUMBER(1),
+    MAX_NUMBER(45),
+    LOTTO_NUMBER_COUNT(6),
+    BONUS_NUMBER_COUNT(1);
+
+    private final int value;
+
+    LottoConstant(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
