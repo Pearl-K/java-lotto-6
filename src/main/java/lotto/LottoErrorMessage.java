c8538176cf4060edac4a5834ed6d6324a97ea16f
Pearl <rkdwlswn227@gmail.com>
2023-11-04 17:33:19 +0900

    feat: LottoErrorMessage 로또 기능에 쓰이는 에러 메세지 enum 클래스로 관리

---
+++ src/main/java/lotto/LottoErrorMessage.java
@@ -0,0 +1,18 @@
+package lotto;
+
+public enum LottoErrorMessage {
+    INVALID_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
+    INVALID_NUMBER_RANGE("로또 번호는 1에서 45 사이어야 합니다."),
+    DUPLICATE_NUMBER("로또 번호에 중복된 숫자가 있습니다."),
+    INVALID_BONUS_NUMBER("보너스 번호는 1에서 45 사이어야 합니다.");
+
+    private final String message;
+
+    LottoErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
