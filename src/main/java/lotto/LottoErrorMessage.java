fe7b326a783b38ec265023a9d1f5dceaae75104f
Pearl <rkdwlswn227@gmail.com>
2023-11-04 17:49:20 +0900

    refactor: 예외 사항 추가

--- src/main/java/lotto/LottoErrorMessage.java
+++ src/main/java/lotto/LottoErrorMessage.java
@@ -4,7 +4,8 @@ public enum LottoErrorMessage {
     INVALID_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
     INVALID_NUMBER_RANGE("로또 번호는 1에서 45 사이어야 합니다."),
     DUPLICATE_NUMBER("로또 번호에 중복된 숫자가 있습니다."),
-    INVALID_BONUS_NUMBER("보너스 번호는 1에서 45 사이어야 합니다.");
+    INVALID_BONUS_NUMBER_COUNT("보너스 번호는 1개여야 합니다."),
+    INVALID_BONUS_NUMBER_RANGE("보너스 번호는 1에서 45 사이어야 합니다.");
 
     private final String message;
 
