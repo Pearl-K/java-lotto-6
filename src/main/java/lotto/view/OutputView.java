f15acf1dfa5bcfdcf11d419e3d6c0fc8fa2847b4
Pearl <rkdwlswn227@gmail.com>
2023-11-08 21:54:20 +0900

    feat: OutputView 출력 기능 세팅

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,15 @@
 package lotto.view;
 
+import static lotto.view.ViewErrorMessage.NUMBER_OF_LOTTO_PURCHASED_MESSAGE;
+
+import java.util.List;
+
 public class OutputView {
+    public static void printPurchaseResult(int numberOfLottoPurchased) {
+        System.out.println(String.format(NUMBER_OF_LOTTO_PURCHASED_MESSAGE.getMessage(), numberOfLottoPurchased));
+    }
+
+    public static void printLottoNumbers(List<List<Integer>> lottoNumbers) {
+        lottoNumbers.forEach(System.out::println);
+    }
 }
