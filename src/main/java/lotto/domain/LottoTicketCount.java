8a1adfc1456697e9a9b9d2cb2bc2b9cbc9a3eed2
Pearl <rkdwlswn227@gmail.com>
2023-11-08 16:05:07 +0900

    refactor: LottoTicketCount에 1000으로 나누기 validate 메서드 추가

--- src/main/java/lotto/domain/LottoTicketCount.java
+++ src/main/java/lotto/domain/LottoTicketCount.java
@@ -1,9 +1,18 @@
 package lotto.domain;
 
 import static lotto.domain.LottoConstant.LOTTO_PRICE;
+import static lotto.view.ViewErrorMessage.ERROR_INVALID_PRICE;
+import static lotto.view.ViewErrorMessage.ERROR_MESSAGE_HEADER;
 
 public class LottoTicketCount {
     public int calculateNumberOfLottoTickets(int purchaseMoney) {
+        validateThousandDivision(purchaseMoney);
         return purchaseMoney / LOTTO_PRICE.getValue();
+    }
+
+    private static void validateThousandDivision(int purchaseMoney) {
+        if (purchaseMoney % LOTTO_PRICE.getValue() != 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER.getMessage() + ERROR_INVALID_PRICE.getMessage());
+        }
     }
 }
