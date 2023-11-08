e5907db9669760a805bfa5c04e496695cd87f360
Pearl <rkdwlswn227@gmail.com>
2023-11-08 16:05:30 +0900

    add: run() 기능 추가

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,12 +1,24 @@
 package lotto.controller;
 
-import lotto.domain.LottoGame;
+import java.util.List;
+import lotto.domain.LottoRandomNumberGenerator;
+import lotto.domain.LottoTicketCount;
 import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class LottoGameController {
 
     public void run() {
+        int purchaseMoney = InputView.inputLottoPurchaseMoney();
+
+        LottoTicketCount ticketCount = new LottoTicketCount();
+        int numberOfLottoTickets = ticketCount.calculateNumberOfLottoTickets(purchaseMoney);
+
+        LottoRandomNumberGenerator randomLottos = new LottoRandomNumberGenerator();
+        List<List<Integer>> randomLottoNumbers = randomLottos.generateRandomNumbers(numberOfLottoTickets);
+
+        InputView.inputLottoWinningNumber();
+
+        int bonusNumber = InputView.inputBonusNumber();
 
     }
 
