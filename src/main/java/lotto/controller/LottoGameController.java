21898535abed6678e3e0c8ffe15b812b809b96c8
Pearl <rkdwlswn227@gmail.com>
2023-11-08 14:17:22 +0900

    feat: LottoGameController 기본 세팅

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,4 +1,17 @@
 package lotto.controller;
 
+import lotto.domain.LottoGame;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class LottoGameController {
+
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        LottoGameController controller = new LottoGameController();
+        controller.run();
+    }
 }
